12e20b21dca726051ea976a5f67fe4bb3bb22219
mizutaki <mizutaki4@gmail.com>
2017-02-04 14:37:54 +0900

    Add fileupload.

--- 02/rails_file_dnd/app/controllers/static_pages_controller.rb
+++ 02/rails_file_dnd/app/controllers/static_pages_controller.rb
@@ -1,4 +1,20 @@
 class StaticPagesController < ApplicationController
   def home
+    @img_paths = []
+    jpgs = Dir.glob(Rails.root.join('public', '*.jpg'))
+    jpgs.each do |j|
+      @img_paths.push('/'+ File.basename(j))
+    end
+  end
+
+  def file_upload
+    params['file'].original_filename
+    file = params['file'].tempfile
+    read_file = File.open(file, 'rb')
+    file_path = 'public/' + params['file'].original_filename
+    File.open(file_path, 'wb') do |f|
+      f.write read_file.read
+    end
+    redirect_to action: 'home', status: :ok
   end
 end
